0ece18d8771a29add94aec0cb313aed18ab94189
mememejlc <jeremy.cook.llb@live.com>
2016-07-29 18:27:51 +1200

    adding javascript api call

---
+++ app.js
@@ -0,0 +1,12 @@
+
+jQuery(document).ready(function($) {
+  $.ajax({
+  url : "http://api.wunderground.com/api/31b66c553f1a25dc/geolookup/conditions/q/IA/Cedar_Rapids.json",
+  dataType : "jsonp",
+  success : function(parsed_json) {
+  var location = parsed_json['location']['city'];
+  var temp_f = parsed_json['current_observation']['temp_f'];
+  alert("Current temperature in " + location + " is: " + temp_f);
+  }
+  });
+});
